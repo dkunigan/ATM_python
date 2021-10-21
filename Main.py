fbf58dfac293fa8d9ff2feb2cd2ec09b10daa25d
dkunigan <kunigan_daniel@columbusstate.edu>
2021-10-21 22:08:56 +0000

    initial main commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+# Welcome to our ATM Software :)
+
+# halp idk where to start
+# We need to create classes
+# Then we can create the functions that draw from those classes
+# We also need a parser or something, possibly buttons if we want a graphical user interface
+
+
